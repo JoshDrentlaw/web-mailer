5615627f531af65fc1a12110eb415b877b8b09de
JoshDrentlaw <joshdrentlaw@gmail.com>
2019-02-20 18:07:32 -0800

    Implemented basic form. Working on file upload. Form redirects and is ready for another message.

--- server.js
+++ server.js
@@ -1,41 +1,57 @@
 const path = require('path');
 const fs = require('fs');
 
+require('dotenv').config();
 const bodyParser = require('body-parser');
 const formidable = require('formidable');
-const nodemailer = require('nodemailer');
+const nodeoutlook = require('nodejs-nodemailer-outlook');
 const express = require('express');
 
 const server = express();
 const port = process.env.PORT || 3000;
 
-const transporter = nodemailer.createTransport({
-    host: 'smtp.office365.com',
-    port: 587,
-    secure: true,
-    auth: {
-        user: process.env.USER,
-        pass: process.env.PASS
-    },
-    tls: { ciphers: 'SSLv3' }
-});
-
-transporter.verify((err, suc) => {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        console.log("Server is ready to take our message.");
-    }
-});
-
+server.use(bodyParser.json());
 server.use(express.static(path.join(__dirname, 'client')));
 
 server.post('/fileupload', (req, res) => {
+    console.log(req.body);
     let form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files) => {
-        let filepath = files.fileupload.path;
+    let filepath = new Promise((res, rej) => {
+        form.parse(req, (err, fields, files) => {
+            res(files.fileupload.path);
+        });
     });
+    
+    filepath.then(path => {
+        let customer;
+        let filename = req.body.filename;
+        let isSame = req.body.isSame;
+        let subject = (isSame) ? req.body.filename : req.body.subject;
+
+        switch (req.body.customer) {
+            case 'haskel':
+                customer = process.env.HASKEL;
+            default:
+                customer = process.env.USER;
+        }
+        nodeoutlook.sendEmail({
+            auth: {
+                user: process.env.USER,
+                pass: process.env.PASS
+            },
+            from: process.env.USER,
+            to: process.env.USER,
+            subject: subject,
+            text: 'Documents attached.',
+            attachments: [
+                {
+                    filename: filename,
+                    path: path
+                }
+            ]
+        });
+    });
+    res.redirect('index.html');
 });
 
 server.listen(port, () => {
